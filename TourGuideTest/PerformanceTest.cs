a3d4d363c093694f74d96d40096346a4ae53eaf7
Benoit <bmarechal@vpauto.eu>
2025-05-13 11:11:35 +0200

    Set preformance tests to 1

--- TourGuideTest/PerformanceTest.cs
+++ TourGuideTest/PerformanceTest.cs
@@ -48,7 +48,7 @@ namespace TourGuideTest
         public async Task HighVolumeTrackLocation()
         {
             //On peut ici augmenter le nombre d'utilisateurs pour tester les performances
-            _fixture.Initialize(100000);
+            _fixture.Initialize(1);
 
             List<User> allUsers = await _fixture.TourGuideService.GetAllUsers();
 
@@ -71,7 +71,7 @@ namespace TourGuideTest
         public async Task HighVolumeGetRewards()
         {           
             // On peut ici augmenter le nombre d'utilisateurs pour tester les performances
-            _fixture.Initialize(100);
+            _fixture.Initialize(1);
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
             var attractions = await _fixture.GpsUtil.GetAttractions();
